e91ef64ebd6348ac548a8f4353a5a8c6d9a191e5
PIN SHUAI <pin.shuai@usu.edu>
2023-07-27 14:05:24 -0600

    updated version 0.0.6 --> 0.0.7

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [ ]
 
 setup(
     author="Pin Shuai",
-    author_email='pinshuai.phd@gmail.com',
+    author_email='pin.shuai@usu.edu',
     python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/pinshuai/modvis',
-    version='0.0.6',
+    version='0.0.7',
     zip_safe=False,
 )
